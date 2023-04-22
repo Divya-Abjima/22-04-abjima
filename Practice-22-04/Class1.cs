00950e671aeda5d0afba6a8eb532d478edaf5557
Divya-Abjima <121873267+Divya-Abjima@users.noreply.github.com>
2023-04-22 17:27:21 +0530

    enum, struct, classes and inheritance in classes virual and override

---
+++ Practice-22-04/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_22_04
+{
+    public class Class1
+    {
+
+        public virtual void printName(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+
+    public class ChildClass : Class1
+    {
+        public static  void greeting(string name)
+        {
+            Console.WriteLine("Hello "+name);
+        }
+    }
+    public class ChildClass2 : Class1
+    {
+        public override void printName(string name)
+        {
+            name = Console.ReadLine();
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ Practice-22-04/Practice.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Practice_22_04
+{
+    public static class Practice
+    {
+        //enum 
+        static void Main(string[] args)
+        {
+            StudentInfo studentInfo= new StudentInfo();
+            Student student= new Student();
+            student.RollNo = 101;
+            student.Name = "John";
+            student.Grade = "XII";
+            student.Address = "Denver, Colorado";
+            student.Phone = 1567382921;
+            Console.WriteLine(student.RollNo);
+            Console.WriteLine(studentInfo);
+            string name = "Divya";
+            ChildClass2.printName(name);
+            ChildClass.greeting(name);
+        }
+    }
+    public struct Student
+    {
+        public int RollNo;
+        public string Name;
+        public string Grade;
+        public long Phone;
+        public string Address;
+    }
+    public enum StudentInfo
+    {
+        Name,
+        Grade,
+        Address,
+        Phone
+    }
+}
