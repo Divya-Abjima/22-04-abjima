6b0e10511357c881b8fcb9e28e1804d567a57c0e
Rajan-abjima <125352988+Rajan-abjima@users.noreply.github.com>
2023-04-22 19:20:23 +0530

    22-04-23 - Arrays and Collection in C# IN PROGRESS

---
+++ Working with Arrays and Collections in C# 9/Demos.BusRoute.FindBusTo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Demos.BusRoute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusRoute[] routes = BusRouteRepository.InitializeRoutes();
+            Console.WriteLine("Where do you want to go?");
+            string location = Console.ReadLine();
+
+            BusRoute route = FindBusTo(routes, location);
+
+            if (route != null )
+                Console.WriteLine($"You can use route {route}");
+            else
+                Console.WriteLine($"No routes go to {location}");
+        }
+
+        public static BusRoute FindBusTo(BusRoute[] routes, string location)
+        {
+            foreach (BusRoute route in routes)
+            {
+                if (route.Origin == location || route.Destination == location)
+                    return route;
+            }
+            return null;
+        }
+    }
+}
---
+++ Working with Arrays and Collections in C# 9/Demos.BusRoute/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Demos.BusRoute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusRoute route40 = new BusRoute(40, "Morecambe", "Preston");
+            BusRoute route42 = new BusRoute(42, "Lancaster", "Blackpool");
+
+            //BusRoute[] routes =
+            //{
+            //    route40,
+            //    route42,
+            //    new BusRoute(100, "University", "Morecambe"),
+            //    new BusRoute(555, "Lancaster", "Keswick")
+            //};
+
+            BusRoute[] routes = new BusRoute[4];
+
+            routes[0] = route40;
+            routes[1] = route42;
+            routes[2] = new BusRoute(100, "University", "Morecambe");
+            routes[3] = new BusRoute(555, "Lancaster", "Keswick");
+            //Console.WriteLine(route40);
+            //Console.WriteLine(route42);
+
+            for (int i = 0; i < routes.Length; i++)
+                Console.WriteLine($"{i}: {routes[i]}");
+
+            //BusRoute firstRoute = routes[0];
+            //Console.WriteLine($"The third route is {routes[2]}");
+            //BusRoute lastRoute = routes[^1];
+            //Console.WriteLine($"the last route is {lastRoute}");
+            //foreach (BusRoute route in routes)
+            //    Console.WriteLine(route);
+        }
+    }
+}
